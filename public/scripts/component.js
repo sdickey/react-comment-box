51db7da2ff7f929be605d1a474538cf5232fb902
sdickey <sean.dickey@live.com>
2016-04-17 10:08:03 -0700

    Added reactive state to comments.

--- public/scripts/component.js
+++ public/scripts/component.js
@@ -1,10 +1,33 @@
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  getInitialState: function() {
+    return { data: [] };
+  },
+
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+
   render: function () {
     return (
       <div className="CommentBox">
         What is up, Sean? I am a freaking comment box, yo!
         <h1>Comments</h1>
-        <CommentList data={this.props.data}/>
+        <CommentList data={this.state.data}/>
         <CommentForm />
       </div>
     );
@@ -65,6 +88,6 @@ var data = [
 
 
 ReactDOM.render (
-  <CommentBox data={data} />,
+  <CommentBox url="/api/comments" />,
   document.getElementById('content')
 )
